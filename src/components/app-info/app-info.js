9a7241cb222b384353439dc5ec2829c560986a07
Alexander <akrimec@gmail.com>
2023-07-11 12:09:40 +0300

    init

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting for employees in company F</h1>
+            <h2>Total number of employees: </h2>
+            <h2>The award will be received: </h2>
+        </div>
+    );
+}
+
+export default AppInfo;
